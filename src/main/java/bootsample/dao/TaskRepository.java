be942c407185b3fba9034e4073deda991cf7f4c3
Marco Roda <marcoroda20@gmail.com>
2018-03-19 16:02:42 +0100

    Added support for adding/listing/deleting/getByemail for users database.

--- src/main/java/bootsample/dao/TaskRepository.java
+++ src/main/java/bootsample/dao/TaskRepository.java
@@ -1,7 +1,6 @@
 package bootsample.dao;
 
 import org.springframework.data.repository.CrudRepository;
-
 import bootsample.model.Task;
 
 public interface TaskRepository extends CrudRepository<Task, Integer>{
